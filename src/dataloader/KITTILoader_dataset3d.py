700e7904e7956e57ffd0b908ab6970e23b7241c6
Sean Seung Won Lee <swdev1202@gmail.com>
2020-04-04 22:50:14 -0700

    changing the crop size of the validation

--- src/dataloader/KITTILoader_dataset3d.py
+++ src/dataloader/KITTILoader_dataset3d.py
@@ -111,8 +111,8 @@ class myImageFloder(data.Dataset):
             if(self.argo):
                 w, h = left_img.size
 
-                left_img = left_img.crop((w-608, h-512), w, h)
-                right_img = right_img.crop((w-608, h-512), w, h)
+                left_img = left_img.crop((w-608, h-512, w, h))
+                right_img = right_img.crop((w-608, h-512, w, h))
                 dataL = dataL[h-512:h, w-512:w]
 
                 left_img = self.transform(left_img)
