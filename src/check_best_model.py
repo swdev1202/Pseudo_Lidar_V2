1040fc676e452cefef4986db220ae4dd9abeedcd
Sean Seung Won Lee <swdev1202@gmail.com>
2020-04-13 15:40:11 -0700

    check model

--- src/check_best_model.py
+++ src/check_best_model.py
@@ -7,7 +7,7 @@ model_path = '/home/011505052/thesis/Pseudo_Lidar_V2/results/sdn_argo_train/ckpt
 model_list = os.listdir(model_path)
 
 for idx, model in enumerate(model_list):
-    checkpoint = torch.load(model)
+    checkpoint = torch.load(model_path + model)
     best_RMSE = checkpoint['best_RMSE']
     print(f'{idx}th checkpoint ... name {model}')
     print(best_RMSE)
